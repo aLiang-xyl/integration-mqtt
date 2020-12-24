1e7f4b5df64a9973d403108471d2a526e4608cae
aLiang <472505858@qq.com>
2020-12-24 15:03:24 +0800

    添加动态主题demo

--- integration-mqtt-demo/src/main/java/com/framework/example/controller/MqttController.java
+++ integration-mqtt-demo/src/main/java/com/framework/example/controller/MqttController.java
@@ -3,6 +3,8 @@ package com.framework.example.controller;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,12 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RestController
 public class MqttController {
+	
+	/**
+	 * 自动注入规则：${channelName}MqttChannelAdapter
+	 */
+	@Autowired
+	private MqttPahoMessageDrivenChannelAdapter channel1MqttChannelAdapter;
 
 
 	@GetMapping(value = { "/sendMessage" })
@@ -38,5 +46,12 @@ public class MqttController {
 		log.info("测试信息 {} ", messageContent);
 		MqttUtils.sendMessage(topic, messageContent, channelName);
 		return messageContent;
+	}
+	
+	@GetMapping(value = { "/addTopic" })
+	public String sendMessageByChannel(String topic, Integer qos) {
+		log.info("主题 ：{} qos：{}", topic, qos);
+		channel1MqttChannelAdapter.addTopic(topic, qos);
+		return "操作成功";
 	}
 }
