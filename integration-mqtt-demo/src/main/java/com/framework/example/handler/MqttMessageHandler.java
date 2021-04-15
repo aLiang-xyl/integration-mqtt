1ec0737027b51ed20b7df1c4a4448b3d24612ec4
aLiang <aLiang-xyl@qq.com>
2021-04-15 13:37:10 +0800

    添加异常事件回调

--- integration-mqtt-demo/src/main/java/com/framework/example/handler/MqttMessageHandler.java
+++ integration-mqtt-demo/src/main/java/com/framework/example/handler/MqttMessageHandler.java
@@ -1,6 +1,10 @@
 package com.framework.example.handler;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
@@ -17,6 +21,19 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Component
 public class MqttMessageHandler implements MessageHandler {
+	
+	/**
+	 * 自动注入规则：${channelName}MqttChannelAdapter
+	 */
+	@Autowired
+	private MqttPahoMessageDrivenChannelAdapter channel1MqttChannelAdapter;
+	
+	@PostConstruct
+	private void addFailEvent() {
+		channel1MqttChannelAdapter.setApplicationEventPublisher((event) -> {
+			log.error("失败事件 {}", event);
+		});
+	}
 	
 	@ServiceActivator(inputChannel = "channel1")
 	@Override
